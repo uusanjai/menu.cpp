7aaaf1e50e2267434b2017902e75ea0dc709da67
uusanjai <c030324034@mahasiswa.poliban.ac.id>
2025-05-12 19:06:42 +0800

    tampilan array

--- menu.cpp
+++ menu.cpp
@@ -2,67 +2,97 @@
 #include <conio.h>
 using namespace std;
 
+void insertion_sort(int data[], int n) {
+    int temp, j;
+    for (int i = 1; i < n; i++) {
+        temp = data[i];
+        j = i - 1;
+        while (j >= 0 && data[j] > temp) {
+            data[j + 1] = data[j];
+            j--;
+        }
+        data[j + 1] = temp;
+    } 
+}
+
+
+void masukkanarray(int data[], int &n) {
+    system("cls");
+    cout << "Masukkan jumlah elemen: ";
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cout << "Elemen ke-" << i + 1 << ": ";
+        cin >> data[i];
+    }
+    getch();
+}
+
+
 
 void dMenu(){
-system("cls");
-cout<<"Aplikasi Tampilan Menu"<<"\n";       
-cout<<"1. Menu Pertama"<<"\n";    //input         
-cout<<"2. Menu Kedua"<<"\n";          //output  
-cout<<"3. Menu Ketiga"<<"\n";          //sorting 
-cout<<"4. Menu Keempat"<<"\n";          //informasi  
-cout<<"5. Exit"<<"\n";            //exit
-cout<<"Masukan angka :";        
 
+    system("cls");
+    cout<<"Aplikasi Tampilan Menu"<<"\n";
+    cout << "1. Masukkan array "<<"\n";   
+    cout << "2. Tampilkan Array" << "\n";
+    cout << "3. Urutkan Array (Insertion Sort)" << "\n";
+    cout << "4. Informasi" << "\n";
+    cout << "5. Exit" << "\n";
+    cout << "Masukkan angka 1-5 : ";
 }
 
-void mPertama(string pesan){
-system("cls");
-cout<<"hallo saya menu "<<pesan;
-getch();
-}
-void info(string pesan){
-system("cls");
-cout<<"hay gean"<<pesan;
-getch();
+void tampilkanArray(int data[], int n) {
+    cout << "Isi array: ";
+    for (int i = 0; i < n; i++) {
+        cout << data[i] << " ";
+    }
+    cout << endl;
+    getch();
 }
 
+void info() {
+    system("cls");
+    cout << "4 SEHAT 5 GAK NAMBAH 6 GAK MUNGKIN 15 MUSTAHILLLL:(  " << endl;
+    getch();
+}
 
 int main() {
-char pl;
-do
-{
-    dMenu();
-    pl=getch();
-  switch (pl)
-  {
-   case '1':
-    /* code */
-    mPertama("pertama");
-    break;
-   case '2':
-    mPertama("ke- dua");
-    /* code */ 
-    break;  
-   case '3':
-    mPertama("ke- tiga");
-    /* code */
-    break;  
-   case '4':
-    info();
-    /* code */
-    break;  
-  case '5':
-    /* code */
-    break;
-  
-  default:
-    system("cls");
-    cout<<"Pilihan Tidak Tersedia";
-    getch();
-    break;
-  }
-
-
-} while (pl!='5');
-  return 0;
+    int data[100], n = 0;
+    char pl;
+    do {
+        dMenu();
+        pl = getch();
+        switch (pl) {
+            case '1':
+                system("cls");
+                masukkanarray(data, n);
+                getch();
+                break;
+            case '2':
+                system("cls");
+                cout << "Array: ";
+                tampilkanArray(data, n);
+                break;
+            case '3':
+                system("cls");
+                insertion_sort(data, n);
+                cout << "Array telah diurutkan." << endl;
+                tampilkanArray(data, n);
+                break;
+            case '4':
+                info();
+                break;
+            case '5':
+                system("cls");
+                cout << "Keluar dari aplikasi" << endl;
+                getch();
+                break;
+            default:
+                system("cls");
+                cout << "Pilihan Tidak Tersedia" << endl;
+                getch();
+                break;
+        }
+    } while (pl != '5');
+    return 0;
 }
